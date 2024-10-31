54548ab457ec34df8337d3adc6dd455e2e7ac201
R M Sivasankar <rmsivasankar@protonmail.com>
2024-11-01 05:05:06 +0530

    AuthJs

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl">
+          HOME
+        </h1>
+      </div>
+    </>
+  );
+}
---
+++ app/components/Navbar.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import {auth, signOut, signIn} from '@/auth'
+
+const Navbar = async() => {
+  const session = await auth();
+
+  return (
+    <header className='px-3 py-3 backdrop-filter backdrop-blur-lg bg-opacity-30 border-b'>
+      <nav className='flex justify-between items-center'>
+        <Link href="/">
+            <h1 className="text-2xl font-bold">DevFlux</h1>
+        </Link>
+
+        <div className="flex items-center gap-5">
+            {session && session?.user ? (
+                <>
+                    <Link href="/startup/create">
+                        <span>Create</span>        
+                    </Link>
+                    <form action={ async() => { 
+                        "use server";
+                        await signOut({ redirectTo: "/" });
+                    }}>
+                        <button type='submit'>
+                            Logout
+                        </button>
+                    </form>
+
+                    <Link href={`/user/${session?.id}`}>
+                        <span>{session?.user?.name}</span>
+                    </Link>
+                </>
+            ) : (
+                <form action={ async() => { 
+                    "use server";
+                    await signIn('github')}}>
+                    <button type='submit'>
+                        Login
+                    </button>
+                </form>
+            )}
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
